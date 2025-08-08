ed710cbecceb1067c4918f678dcc4ecb86898454
SOURAVi0001 <souravpandr@gmail.com>
2025-08-08 11:21:57 +0530

    testing data added

--- route/ContactUs.js
+++ route/ContactUs.js
@@ -3,7 +3,34 @@ const path=require('path');
 const rootdir=require('../Utils/path');
 const home=require('../controllers/home');
 const UserRouter=express.Router();
-
 UserRouter.get('/ContactUs',home.contact_us);
+
+const admindb=require('../models/clubadmin');
+const userdb=require('../models/user');
+UserRouter.get('/testing_data', async (req, res) => {
+    try {
+        // Fetch all users from userdb
+        const users = await userdb.find({}).lean();
+        
+        // Fetch all admins from admindb
+        const admins = await admindb.find({}).lean();
+        
+        // Render the template with both datasets
+        res.render('test_data/test_data', {
+            users: users,
+            admins: admins,
+            title: 'Testing Data Interface'
+        });
+    } catch (error) {
+        console.error('Error fetching testing ', error);
+        res.render('test_data/test_data', {
+            users: [],
+            admins: [],
+            error: 'Error loading data',
+            title: 'Testing Data Interface'
+        });
+    }
+});
+
 
 module.exports=UserRouter;
